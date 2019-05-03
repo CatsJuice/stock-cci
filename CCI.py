bd4aed956f64a54c92e9b0bcff424fde63c84ff8
CatsJuice <wp1298554944@hotmail.com>
2019-05-03 15:27:35 +0800

    catsjuice-2019-05-03-15:27 增加了购买测试

--- CCI.py
+++ CCI.py
@@ -3,11 +3,24 @@ from tqdm import tqdm
 import pandas as pd
 from pandas import Series, DataFrame
 
-# 单只股票的CCI类
-class StockCCI(object):
 
-    def __init__(self):
-        pass
+class BuyInfo(object):
+
+    def __init__(self, code, buy_arr, sold_arr):
+        self.code = code
+        self.buy_date = buy_arr[0]
+        self.buy_close = buy_arr[2]
+        self.sold_date = sold_arr[0]
+        self.sold_close = sold_arr[2]
+        self.diff = self.sold_close - self.buy_close
+
+    def to_string(self):
+        buy_close_ = " "*(6-len(str(self.buy_close))) + str(self.buy_close)
+        sold_close_ = " "*(6-len(str(self.sold_close))) + str(self.sold_close)
+        diff_ = format(self.diff, '.2f')
+        diff_ = " "*(6 - len(diff_)) + diff_
+        return "[%s]在%s日买入, 收盘价为%s, %s日卖出, 收盘价为%s, 差价为%s" % (self.code, \
+            self.buy_date, buy_close_, self.sold_date, sold_close_, diff_)
 
 # TYP:=(HIGH+LOW+CLOSE)/3;
 # CCI:(TYP-MA(TYP,N))/(0.015*AVEDEV(TYP,N));
@@ -82,6 +95,7 @@ class CCIAnalyze(object):
         self.n = n                                      # 公式中的天数， 默认14
         self.end_date = end_date                        # 最早的日期， 默认无下限
         self.all_cci = {}                               # 保存每只股票的 cci
+        self.all_buy_res = []                           # 保存购买的结果
     
     def analyze_all(self):
         file_list = os.listdir(self.file_path_prefix)
@@ -142,12 +156,15 @@ class CCIAnalyze(object):
         self.all_cci[str(self.code)] = stock_cci
         df_new_col = DataFrame(col, columns=['date','code','CCI','close'])
         df_new_col.to_csv(self.cci_path_prefix + str(self.code) + '.csv', mode='w', index=False)    # 存储至csv
-    
+         
     def save_to_txt(self):
         file_handle=open('cci.txt',mode='w', encoding="utf-8")
         file_handle.write(str(self.all_cci))
 
     def test_buy(self):
+        '''
+            < -100 ， 后一天买入， > 100 后一天卖出
+        '''
         print("正在测试购买")
         file_list = os.listdir(self.cci_path_prefix)
         for index in tqdm(range(len(file_list))):
@@ -161,18 +178,101 @@ class CCIAnalyze(object):
             rows = df.values[::-1]  # 倒转数组
             
             flag = False    # 标记是否已找到 < -100 的， 
+            buy = False
+            sold = False
+            buy_arr = []
+            sold_arr = []
             for row in rows:
-                if not flag:
+                # 判断前一天是否满足 <-100可以买入
+                if buy:
+                    buy_arr = [row[0], row[2], row[3]]
+                    if row[2] == 0 or row[3] == 0:      # 为缺失数据
+                        flag = False
+                    buy = False
+                if sold:    
+                    # do Sold
+                    sold_arr = [row[0], row[2], row[3]]
+                    if row[2] != 0 and row[3] != 0:
+                        buy_info = BuyInfo(code, buy_arr, sold_arr)
+                        self.all_buy_res.append(buy_info)
+                    # print(buy_info.to_string())
+                    sold = False
+                    buy_arr = []
+                    sold_arr = []
+                if not flag:        # 没找到 < -100 的， 寻找
                     if row[2] < -100:
-                        pass
+                        buy = True
+                        flag = True
+                else:               # 已经找到 < -100的， 找 > 100的来卖出
+                    if row[2] > 100:
+                        sold = True
+                        flag = False
+        self.show_buy_res()
 
+    def test_buy_2(self):
+        '''
+        # 在达到 <-100 的峰值的后一天买入， > 100 峰值的后一天卖出
+        '''
+        file_list = os.listdir(self.cci_path_prefix)
+        for index in tqdm(range(len(file_list))):
+            filename = file_list[index]     # 取出文件名
+            code = filename[0:6]
+            try:
+                df = pd.read_csv(self.cci_path_prefix + filename, encoding='gbk')
+            except:
+                print('文件'+str(code) + '.csv 打开失败')
+                return False
+            rows = df.values[::-1]  # 倒转数组
+
+            flag = False    # 是否已经找到 < -100的
+            buy = False
+            sold = False
+            buy_arr = []
+            sold_arr = []
+            yesterday_cci = 0
+            for row in rows:
+                if buy:
+                    buy_arr = [row[0], row[2], row[3]]
+                    if row[2] == 0 or row[3] == 0:      # 为缺失数据
+                        flag = False
+                    buy = False
+                if sold:    
+                    # do Sold
+                    sold_arr = [row[0], row[2], row[3]]
+                    if row[2] != 0 and row[3] != 0:
+                        buy_info = BuyInfo(code, buy_arr, sold_arr)
+                        self.all_buy_res.append(buy_info)
+                    # print(buy_info.to_string())
+                    sold = False
+                    buy_arr = []
+                    sold_arr = []
+                if flag:
+                    if row[2] > 100 and row[2] < yesterday_cci:
+                        sold = True
+                        flag = False
+                else:
+                    if row[2] < -100 and row[2] > yesterday_cci:
+                        buy = True
+                        flag = True
+                yesterday_cci = row[2]
+        self.show_buy_res()
+
+    def show_buy_res(self):
+        rate = 0
+        for item in self.all_buy_res:
+            if item.diff > 0:
+                rate += 1
+            print(item.to_string())
+        rate /= len(self.all_buy_res)
+        rate = format(rate*100, '.4f') + '%'
+        print("总共有%s种满足条件的情况， 盈利的几率为%s" % (len(self.all_buy_res), rate))
 
 
 if __name__ == '__main__':
     file_path_prefix = 'F:\\files\\sharesDatas\\kline\\'
     cci_path_prefix = 'F:\\files\\sharesDatas\\cci\\'
     code = '000001'
-    end_date = '2018-01-01'
+    end_date = '2017-01-01'
     cci_analyze = CCIAnalyze(file_path_prefix, cci_path_prefix=cci_path_prefix, end_date=end_date, code=code)
 
     # cci_analyze.analyze_all()
@@ -180,5 +280,5 @@ if __name__ == '__main__':
     #     print(cci)
     # cci_analyze.analyze_all()
 
-    cci_analyze.test_buy()
+    cci_analyze.test_buy_2()
     
